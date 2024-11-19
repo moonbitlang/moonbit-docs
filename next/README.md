accafb238e0f7f93f3c2a43da332f9a1e1ee23b5
zihang <yezihang@idea.edu.cn>
2024-11-19 10:17:47 +0800

    doc(next): build & i18n support

--- next/README.md
+++ next/README.md
@@ -2,10 +2,33 @@
 
 A new MoonBit docs framework based on Sphinx.
 
-## Install
+## Develop
+
+### Install
 
 ```bash
 python3 -m venv .env
 source .env/bin/activate
 pip install -r requirements.txt
+```
+
+### Build
+
+```bash
+make html
+python3 -m http.server -d _build/html
+```
+
+For Chinese version:
+
+```bash
+make -e SPHINXOPTS="-D language='zh_CN'" html
+python3 -m http.server -d _build/html
+```
+
+### Update translation template
+
+```bash
+make gettext
+sphinx-intl update -p _build/gettext -l zh_CN
 ```
---
+++ next/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MoonBit Document'
+copyright = '2024, International Digital Economy Academy'
+author = 'International Digital Economy Academy'
+release = 'v0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
