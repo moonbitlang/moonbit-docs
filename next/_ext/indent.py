87ec789eea624fcebb4f818a6a940e853bfc779b
zihang <yezihang@idea.edu.cn>
2024-12-03 16:50:26 +0800

    fix(i18n): bypass myst parser issue

---
+++ next/_ext/indent.py
@@ -0,0 +1,12 @@
+# https://github.com/executablebooks/MyST-Parser/issues/444
+
+# Workaround suggested in https://github.com/executablebooks/MyST-Parser/issues/444#issuecomment-1179796223
+from sphinx.transforms import i18n
+
+class ModifiedIndent:
+    def __init__(self, s, _):
+        self.s = s
+    def __radd__(self, _):
+        return f"```\n{self.s}\n```"
+
+i18n.indent = ModifiedIndent
