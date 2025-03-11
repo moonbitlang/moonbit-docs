d48b87615fcb2f018f0fa6e8e890ac62b9b93aa2
zihang <yezihang@idea.edu.cn>
2025-03-11 11:06:42 +0800

    fix: add `is` highlight

--- next/_ext/lexer.py
+++ next/_ext/lexer.py
@@ -36,7 +36,7 @@ class MoonBitLexer(RegexLexer):
             (words(('type', 'type!', 'enum', 'struct', 'trait', 'typealias'), suffix=r"\b"), token.Keyword.Declaration),
             (words(('fn', 'const', 'let', 'mut', 'impl', 'with', 'derive'), suffix=r"\b"), token.Keyword.Declaration),
             (words(('self', 'Self'), suffix=r"\b"), token.Keyword),
-            (words(('guard', 'if', 'while', 'match', 'else', 'loop', 'for', 'in'), suffix=r"\b"), token.Keyword),
+            (words(('guard', 'if', 'while', 'match', 'else', 'loop', 'for', 'in', 'is'), suffix=r"\b"), token.Keyword),
             (words(('return', 'break', 'continue'), suffix=r"\b"), token.Keyword),
             (words(('try', 'catch', 'catch!', 'raise'), suffix=r"\b"), token.Keyword),
             (r"\bas\b", token.Keyword),
