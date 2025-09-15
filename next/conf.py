785bfcb05662b188908fc8ead489cc934f08252c
zihang <yezihang@idea.edu.cn>
2025-09-15 13:48:21 +0800

    chore: update syntax

--- next/conf.py
+++ next/conf.py
@@ -9,7 +9,7 @@
 project = 'MoonBit'
 author = 'International Digital Economy Academy'
 copyright = '%Y, {author}'.format(author=author)
-release = 'v0.6.25'
+release = 'v0.6.27'
 
 # I18N based on Readthedocs Environment
 # https://docs.readthedocs.io/en/stable/reference/environment-variables.html
