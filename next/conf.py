34fbfdb842127de72363ec8ecf59c73c43c47c2b
zihang <yezihang@idea.edu.cn>
2026-04-29 16:40:55 +0800

    docs: update examples for MoonBit 0.9.1

--- next/conf.py
+++ next/conf.py
@@ -9,7 +9,7 @@
 project = 'MoonBit'
 author = 'International Digital Economy Academy'
 copyright = '%Y, {author}'.format(author=author)
-release = 'v0.9.0'
+release = 'v0.9.1'
 
 # I18N based on Readthedocs Environment
 # https://docs.readthedocs.io/en/stable/reference/environment-variables.html
