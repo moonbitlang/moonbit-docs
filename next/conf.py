386b3663bfc1cb9886b6832e4cc9d5e9b81b1af3
zihang <yezihang@idea.edu.cn>
2026-05-13 13:53:28 +0800

    docs: add checked E4215 examples

--- next/conf.py
+++ next/conf.py
@@ -9,7 +9,7 @@
 project = 'MoonBit'
 author = 'International Digital Economy Academy'
 copyright = '%Y, {author}'.format(author=author)
-release = 'v0.9.1'
+release = 'v0.9.2'
 
 # I18N based on Readthedocs Environment
 # https://docs.readthedocs.io/en/stable/reference/environment-variables.html
